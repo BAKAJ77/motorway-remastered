1357e6ae6d6b1028b9feca182d7f61df69154c40
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2023-03-08 03:40:26 +0000

    Added rendering system implementation

---
+++ src/graphics/renderer.h
@@ -0,0 +1,36 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <core/window_frame.h>
+#include <core/shader_system.h>
+#include <graphics/camera_3d.h>
+#include <graphics/geometry.h>
+
+class Renderer
+{
+private:
+	Renderer() = default;
+public:
+	enum class ClearFlag : uint32_t
+	{
+		COLOR_BUFFER_BIT = 0x00004000,
+		DEPTH_BUFFER_BIT = 0x00000100,
+		STENCIL_BUFFER_BIT = 0x00000400
+	};
+
+	~Renderer() = default;
+
+	// Initializes the rendering system.
+	void Init() const;
+
+	// Clears the current active framebuffer.
+	void Clear(ClearFlag mask, const glm::vec4& color);
+
+	void Render(const Camera3D& camera, const Geometry& geometry) const;
+
+	static Renderer& GetInstance();
+};
+
+extern Renderer::ClearFlag operator|(const Renderer::ClearFlag& lhs, const Renderer::ClearFlag& rhs);
+
+#endif
